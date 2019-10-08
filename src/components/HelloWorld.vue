fdc5eaee357d502f4b09cfbfd2f930b8f8802606
Dallin <dallinis@hotmail.com>
2019-10-08 11:35:31 -0600

    Completes mobile styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,8 @@
         <h1>DALLIN COONS</h1>
         <h2>Test-Driven Software Engineer</h2>
         <h2>Data Science Student @ BYU - Idaho</h2>
-        <!--<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 652 336.84">-->
-            <!--<title>blue-background</title>-->
-            <!--<polygon class="cls-blue-background" points="652 336.84 0 294.17 0 0 652 0 652 336.84"/>-->
-        <!--</svg>-->
     </div>
-    <div>
+    <div class="projects-wrapper">
         <h3 class="section-title">Projects</h3>
         <div class="project">
             <div class="project-header">
@@ -26,11 +22,7 @@
                 </div>
 
             </div>
-            <p>A little bit about the project</p>
-            <div class="languages">
-                <p class="language-used">PHP</p>
-                <p class="language-used">Vue</p>
-            </div>
+            <p class="project-description">The sky calls to us invent the universe a very small stage in a vast cosmic arena another world another world descended from astronomers and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
         </div>
         <div class="project">
             <div class="project-header">
@@ -47,11 +39,7 @@
                 </div>
 
             </div>
-            <p>A little bit about the project</p>
-            <div class="languages">
-                <p class="language-used">PHP</p>
-                <p class="language-used">Vue</p>
-            </div>
+            <p class="project-description">Hearts of the stars Sea of Tranquility galaxies from which we spring billions upon billions Cambrian explosion. Cosmic fugue the carbon in our apple pies paroxysm of global death Apollonius of Perga gathered by gravity the carbon in our apple pies. Kindling the energy hidden in matter the only home we've ever known stirred by starlight hundreds of thousands network of wormholes extraordinary claims require extraordinary evidence?</p>
         </div>
     </div>
     <div>
@@ -72,29 +60,25 @@
     </div>
     <footer>
         <div class="icon-wrapper">
-            <a class="icon-link">
+            <a class="icon-link" href="https://github.com/dallincoons" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77">
                     <title>github-icon</title>
                     <path class="cls-github" d="M16.29,0a16.29,16.29,0,0,0-5.15,31.75c.82.15,1.11-.36,1.11-.79s0-1.41,0-2.77C7.7,29.18,6.74,26,6.74,26a4.36,4.36,0,0,0-1.81-2.39c-1.47-1,.12-1,.12-1a3.43,3.43,0,0,1,2.49,1.68,3.48,3.48,0,0,0,4.74,1.36,3.46,3.46,0,0,1,1-2.18c-3.62-.41-7.42-1.81-7.42-8a6.3,6.3,0,0,1,1.67-4.37,5.94,5.94,0,0,1,.16-4.31s1.37-.44,4.48,1.67a15.41,15.41,0,0,1,8.16,0c3.11-2.11,4.47-1.67,4.47-1.67A5.91,5.91,0,0,1,25,11.07a6.3,6.3,0,0,1,1.67,4.37c0,6.26-3.81,7.63-7.44,8a3.85,3.85,0,0,1,1.11,3c0,2.18,0,3.94,0,4.47s.29.94,1.12.78A16.29,16.29,0,0,0,16.29,0Z"/>
                 </svg>
             </a>
-            <a class="icon-link">
+            <a class="icon-link" href="https://www.linkedin.com/in/dallin-coons-784a6b34/" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.06 38.06">
-                    <title>linkedin-icon</title>
-                    <path class="cls-linkedin-circle" d="M19,38.06h0A19,19,0,0,1,0,19H0A19,19,0,0,1,19,0h0a19,19,0,0,1,19,19h0A19,19,0,0,1,19,38.06Z"/>
-                    <rect class="cls-linkedin-icon" x="10.4" y="14.79" width="3.94" height="12.72"/>
-                    <path class="cls-linkedin-icon" d="M12.35,13.12A2.35,2.35,0,1,0,10,10.77,2.34,2.34,0,0,0,12.35,13.12Z"/>
-                    <path class="cls-linkedin-icon" d="M20.64,20.83C20.64,19,21.46,18,23,18s2.14,1,2.14,2.85v6.68H29.1v-8c0-3.41-1.93-5.06-4.63-5.06a4.42,4.42,0,0,0-3.83,2.1V14.79H16.86V27.51h3.78Z"/>
+                    <title>linkedin-logo-unfilled</title>
+                    <path class="cls-linkedin-circle" d="M19,0A19,19,0,0,0,0,19H0a19,19,0,0,0,38.06,0h0A19,19,0,0,0,19,0ZM14.34,27.51H10.4V14.79h3.94Zm-2-14.39a2.35,2.35,0,1,1,2.33-2.35A2.34,2.34,0,0,1,12.35,13.12ZM29.1,27.51H25.18V20.83C25.18,19,24.48,18,23,18s-2.4,1.06-2.4,2.85v6.68H16.86V14.79h3.78V16.5a4.42,4.42,0,0,1,3.83-2.1c2.7,0,4.63,1.65,4.63,5.06Z"/>
                 </svg>
             </a>
-            <a class="icon-link">
+            <a class="icon-link" href="https://twitter.com/goodsoftnuggets" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.06 38.06">
-                    <title>twitter-icon</title>
-                    <path class="cls-twitter-cricle" d="M19,38.06h0A19,19,0,0,1,0,19H0A19,19,0,0,1,19,0h0a19,19,0,0,1,19,19h0A19,19,0,0,1,19,38.06Z"/>
-                    <path class="cls-twitter-icon" d="M8.89,25.4a11.83,11.83,0,0,0,18.2-10.51,8.18,8.18,0,0,0,2.08-2.15,8.39,8.39,0,0,1-2.39.66,4.17,4.17,0,0,0,1.83-2.31,8.13,8.13,0,0,1-2.64,1,4.17,4.17,0,0,0-7.09,3.8,11.79,11.79,0,0,1-8.58-4.35,4.18,4.18,0,0,0,1.29,5.56,4.26,4.26,0,0,1-1.88-.52A4.16,4.16,0,0,0,13,20.72a4.22,4.22,0,0,1-1.88.07,4.18,4.18,0,0,0,3.89,2.89A8.37,8.37,0,0,1,8.89,25.4Z"/>
+                    <title>twitter-logo-unfilled</title>
+                    <path class="cls-twitter-cricle" d="M19,0A19,19,0,0,0,0,19H0a19,19,0,0,0,38.06,0h0A19,19,0,0,0,19,0Zm8.06,14.89A11.83,11.83,0,0,1,8.89,25.4a8.37,8.37,0,0,0,6.16-1.72,4.18,4.18,0,0,1-3.89-2.89A4.22,4.22,0,0,0,13,20.72a4.16,4.16,0,0,1-3.33-4.13,4.26,4.26,0,0,0,1.88.52,4.18,4.18,0,0,1-1.29-5.56,11.79,11.79,0,0,0,8.58,4.35A4.17,4.17,0,0,1,26,12.1a8.13,8.13,0,0,0,2.64-1,4.17,4.17,0,0,1-1.83,2.31,8.39,8.39,0,0,0,2.39-.66A8.18,8.18,0,0,1,27.09,14.89Z"/>
                 </svg>
             </a>
-            <a class="icon-link">
+            <a class="icon-link" href="mailto: dallinis@hotmail.com">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
                     <title>email-icon</title>
                     <polygon class="cls-email-icon" points="14.05 18.55 14.05 29.45 19.5 24 14.05 18.55"/>
@@ -105,7 +89,10 @@
                 </svg>
             </a>
         </div>
-        <p>Coyright</p>
+        <div class="resume-button-wrapper">
+            <a class="resume-text" href="/dallin-coons-resume.pdf" target="_blank">Resume</a>
+        </div>
+        <p class="footer-info">Designed and coded by <a href="http://emilydcoons.com" target="_blanks">Emily</a></p>
     </footer>
   </div>
 </template>
@@ -118,6 +105,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+html, body {
+    max-width: 100%;
+    overflow-x: hidden;
+}
 .hello {
     padding: 1rem;
     position: relative;
@@ -129,12 +120,14 @@ h1 {
     font-weight: 700;
     margin: 0;
     margin-bottom: 1rem;
-    /*color: #01ABD1;*/
+    margin-left: -1rem;
+    transform: rotate(-6deg);
 }
 
 h2 {
     margin: 0;
     font-size: 16px;
+    transform: rotate(-6deg);
 }
 
 h3 {
@@ -150,22 +143,27 @@ ul {
 
 .header-section {
     background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");
-    margin: -3rem -3rem 0 -2rem;
-    padding: 2.5rem;
+    margin: -3rem;
+    padding: 3.5rem;
     transform: rotate(6deg);
-}
-
-.project {
-    border: 2px solid #0D0D0D;
-    padding: .5rem;
-    margin-bottom: 1rem;
 }
 
 .section-title {
     text-transform: uppercase;
-    color: #01ABD1;
+    color: #0D0D0D;
     font-size: 1.5rem;
     margin-bottom: .5rem;
+}
+
+.projects-wrapper {
+    margin-top: 5rem;
+}
+
+.project {
+    border: 2px solid #01ABD1;
+    border-bottom: 8px solid #01abd1;
+    padding: .5rem;
+    margin-bottom: 2rem;
 }
 
 .project-header {
@@ -188,16 +186,8 @@ ul {
     width: 50%;
 }
 
-.languages {
-    display: flex;
-}
-
-.language-used {
-    font-style: italic;
-    font-size: .75rem;
-    display: inline-block;
-    margin-right: 1rem;
-    color: #10ABD1;
+.project-description {
+    line-height: 2;
 }
 
 .article {
@@ -207,7 +197,8 @@ ul {
 
 .article a {
     text-decoration: none;
-    color: #0D0D0D;
+    color: #01ABD1;
+    font-weight: 500;
 }
 
 .icon-wrapper {
@@ -228,14 +219,11 @@ ul {
 
 .cls-github{fill:#0D0D0D;
 fill-rule:evenodd;}
-.cls-blue-github{fill:#10ABD1;
+.cls-blue-github{fill:#01ABD1;
     fill-rule:evenodd;}
-.cls-web{fill:#0D0D0D;}
-.cls-blue-web{fill:#10ABD1;}
+.cls-blue-web{fill:#01ABD1;}
 .cls-linkedin-circle{fill:#0D0D0D;}
-.cls-linkedin-icon{fill:#fff;}
 .cls-twitter-cricle{fill:#0D0D0D;}
-.cls-twitter-icon{fill:#fff;}
 .cls-email-icon{fill: #0D0D0D}
 
 
@@ -243,8 +231,34 @@ footer {
     background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");
     margin: 2rem -2rem -1rem -2rem;
     padding: 2.5rem;
-    /*transform: rotate(-6deg);*/
-    /*position: absolute;*/
-    /*bottom: 0;*/
+    display: grid;
+    justify-items: center;
+    grid-gap: 10px;
+}
+
+.resume-button-wrapper {
+    border: 2px solid #0d0d0d;
+    margin-top: 1rem;
+    padding: 1rem 2rem;
+    text-transform: uppercase;
+    font-weight: 600;
+}
+
+.footer-info {
+    font-style: italic;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 1rem;
+}
+.footer-info a {
+    color: #0D0D0D;
+    font-weight: 700;
+    text-decoration: none;
+}
+
+.resume-text {
+    color: #0D0D0D;
+    font-weight: 700;
+    text-decoration: none;
 }
 </style>
