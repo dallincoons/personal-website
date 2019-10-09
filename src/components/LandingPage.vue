18fc51a262d1e26d85799ed37f10084d06585dd1
Dallin <dallinis@hotmail.com>
2019-10-09 17:30:04 -0600

    Finishes desktop styling and adds media queries

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,48 +1,32 @@
 <template>
   <div class="main-wrapper">
     <div class="header-section">
-        <h1>DALLIN COONS</h1>
-        <h2>Test-Driven Software Engineer</h2>
-        <h2>Data Science Student @ BYU - Idaho</h2>
-        <div class="resume-button-wrapper">
+        <h1 class="title">DALLIN COONS</h1>
+        <h2 class="sub-title sub-title-1">Test-Driven Software Engineer</h2>
+        <h2 class="sub-title sub-title-2">Data Science Student @ BYU - Idaho</h2>
+        <div class="resume-button-wrapper hvr-sweep-to-right lg-screen-resume">
             <a class="resume-text" href="/dallin-coons-resume.pdf" target="_blank">Resume</a>
         </div>
     </div>
     <div class="section-wrapper">
         <h3 class="section-title">Projects</h3>
         <div class="project">
-            <div class="project-header">
+            <div>
                 <h4 class="project-title">Pepper Rodeo</h4>
-                <div class="project-links">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77">
-                            <title>github-icon</title>
-                            <path class="cls-blue-github" d="M16.29,0a16.29,16.29,0,0,0-5.15,31.75c.82.15,1.11-.36,1.11-.79s0-1.41,0-2.77C7.7,29.18,6.74,26,6.74,26a4.36,4.36,0,0,0-1.81-2.39c-1.47-1,.12-1,.12-1a3.43,3.43,0,0,1,2.49,1.68,3.48,3.48,0,0,0,4.74,1.36,3.46,3.46,0,0,1,1-2.18c-3.62-.41-7.42-1.81-7.42-8a6.3,6.3,0,0,1,1.67-4.37,5.94,5.94,0,0,1,.16-4.31s1.37-.44,4.48,1.67a15.41,15.41,0,0,1,8.16,0c3.11-2.11,4.47-1.67,4.47-1.67A5.91,5.91,0,0,1,25,11.07a6.3,6.3,0,0,1,1.67,4.37c0,6.26-3.81,7.63-7.44,8a3.85,3.85,0,0,1,1.11,3c0,2.18,0,3.94,0,4.47s.29.94,1.12.78A16.29,16.29,0,0,0,16.29,0Z"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.73 31.73">
-                            <title>website-icon</title>
-                            <path class="cls-blue-web" d="M30.85,21.09a15.92,15.92,0,0,0,0-10.44.88.88,0,0,0-.05-.15,15.87,15.87,0,0,0-29.87,0l-.05.15a15.92,15.92,0,0,0,0,10.44l.05.14a15.87,15.87,0,0,0,29.87,0,.78.78,0,0,0,.05-.14Zm-15,8.37c-.61,0-1.58-1.09-2.4-3.54a25.24,25.24,0,0,1-.93-4H19.2a25.2,25.2,0,0,1-.94,4c-.82,2.45-1.79,3.54-2.39,3.54Zm-3.61-9.82c-.11-1.21-.17-2.48-.17-3.77s.06-2.56.17-3.78h7.21c.11,1.22.17,2.48.17,3.78s-.06,2.56-.17,3.77Zm-10-3.77a13.49,13.49,0,0,1,.54-3.78H10c-.11,1.24-.16,2.51-.16,3.78s0,2.53.16,3.77H2.81a13.42,13.42,0,0,1-.54-3.77Zm13.6-13.6c.6,0,1.57,1.1,2.39,3.54a25.47,25.47,0,0,1,.94,4H12.54a25.24,25.24,0,0,1,.93-4c.82-2.44,1.79-3.54,2.4-3.54Zm5.88,9.82h7.18a13.71,13.71,0,0,1,0,7.55H21.75c.11-1.24.16-2.51.16-3.77s-.05-2.54-.16-3.78ZM28,9.82H21.49a21.78,21.78,0,0,0-2.07-7.07A13.66,13.66,0,0,1,28,9.82ZM12.31,2.75a22,22,0,0,0-2.07,7.07H3.7a13.62,13.62,0,0,1,8.61-7.07ZM3.7,21.91h6.54A22.1,22.1,0,0,0,12.31,29,13.66,13.66,0,0,1,3.7,21.91ZM19.42,29a21.85,21.85,0,0,0,2.07-7.08H28A13.7,13.7,0,0,1,19.42,29Z"/>
-                        </svg>
-                </div>
-
+                <p class="project-description">
+                    The sky calls to us invent the universe a very small stage in a vast cosmic arena another world another world descended from astronomers and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                </p>
             </div>
-            <p class="project-description">The sky calls to us invent the universe a very small stage in a vast cosmic arena another world another world descended from astronomers and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+            <a class="project-more">More Info</a>
         </div>
-        <div class="project">
-            <div class="project-header">
+        <div class="project ">
+            <div>
                 <h4 class="project-title">Brainframe Recall</h4>
-                <div class="project-links">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77">
-                        <title>github-icon</title>
-                        <path class="cls-blue-github" d="M16.29,0a16.29,16.29,0,0,0-5.15,31.75c.82.15,1.11-.36,1.11-.79s0-1.41,0-2.77C7.7,29.18,6.74,26,6.74,26a4.36,4.36,0,0,0-1.81-2.39c-1.47-1,.12-1,.12-1a3.43,3.43,0,0,1,2.49,1.68,3.48,3.48,0,0,0,4.74,1.36,3.46,3.46,0,0,1,1-2.18c-3.62-.41-7.42-1.81-7.42-8a6.3,6.3,0,0,1,1.67-4.37,5.94,5.94,0,0,1,.16-4.31s1.37-.44,4.48,1.67a15.41,15.41,0,0,1,8.16,0c3.11-2.11,4.47-1.67,4.47-1.67A5.91,5.91,0,0,1,25,11.07a6.3,6.3,0,0,1,1.67,4.37c0,6.26-3.81,7.63-7.44,8a3.85,3.85,0,0,1,1.11,3c0,2.18,0,3.94,0,4.47s.29.94,1.12.78A16.29,16.29,0,0,0,16.29,0Z"/>
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.73 31.73">
-                        <title>website-icon</title>
-                        <path class="cls-blue-web" d="M30.85,21.09a15.92,15.92,0,0,0,0-10.44.88.88,0,0,0-.05-.15,15.87,15.87,0,0,0-29.87,0l-.05.15a15.92,15.92,0,0,0,0,10.44l.05.14a15.87,15.87,0,0,0,29.87,0,.78.78,0,0,0,.05-.14Zm-15,8.37c-.61,0-1.58-1.09-2.4-3.54a25.24,25.24,0,0,1-.93-4H19.2a25.2,25.2,0,0,1-.94,4c-.82,2.45-1.79,3.54-2.39,3.54Zm-3.61-9.82c-.11-1.21-.17-2.48-.17-3.77s.06-2.56.17-3.78h7.21c.11,1.22.17,2.48.17,3.78s-.06,2.56-.17,3.77Zm-10-3.77a13.49,13.49,0,0,1,.54-3.78H10c-.11,1.24-.16,2.51-.16,3.78s0,2.53.16,3.77H2.81a13.42,13.42,0,0,1-.54-3.77Zm13.6-13.6c.6,0,1.57,1.1,2.39,3.54a25.47,25.47,0,0,1,.94,4H12.54a25.24,25.24,0,0,1,.93-4c.82-2.44,1.79-3.54,2.4-3.54Zm5.88,9.82h7.18a13.71,13.71,0,0,1,0,7.55H21.75c.11-1.24.16-2.51.16-3.77s-.05-2.54-.16-3.78ZM28,9.82H21.49a21.78,21.78,0,0,0-2.07-7.07A13.66,13.66,0,0,1,28,9.82ZM12.31,2.75a22,22,0,0,0-2.07,7.07H3.7a13.62,13.62,0,0,1,8.61-7.07ZM3.7,21.91h6.54A22.1,22.1,0,0,0,12.31,29,13.66,13.66,0,0,1,3.7,21.91ZM19.42,29a21.85,21.85,0,0,0,2.07-7.08H28A13.7,13.7,0,0,1,19.42,29Z"/>
-                    </svg>
-                </div>
-
+                <p class="project-description">
+                    Hearts of the stars Sea of Tranquility galaxies from which we spring billions upon billions Cambrian explosion.
+                </p>
             </div>
-            <p class="project-description">Hearts of the stars Sea of Tranquility galaxies from which we spring billions upon billions Cambrian explosion.</p>
+            <a class="project-more">More Info</a>
         </div>
     </div>
     <div class="section-wrapper">
@@ -83,7 +67,7 @@
         <div class="email-line"></div>
     </div>
     <div class="email-wrapper">
-        <a href="mailto: dallinis@hotmail.com">dallinis@hotmail.com</a>
+        <a href="mailto: dallincoons@gmail.com">dallincoons@gmail.com</a>
         <div class="email-line"></div>
     </div>
     <footer>
@@ -106,7 +90,7 @@
                     <path class="cls-twitter-cricle" d="M19,0A19,19,0,0,0,0,19H0a19,19,0,0,0,38.06,0h0A19,19,0,0,0,19,0Zm8.06,14.89A11.83,11.83,0,0,1,8.89,25.4a8.37,8.37,0,0,0,6.16-1.72,4.18,4.18,0,0,1-3.89-2.89A4.22,4.22,0,0,0,13,20.72a4.16,4.16,0,0,1-3.33-4.13,4.26,4.26,0,0,0,1.88.52,4.18,4.18,0,0,1-1.29-5.56,11.79,11.79,0,0,0,8.58,4.35A4.17,4.17,0,0,1,26,12.1a8.13,8.13,0,0,0,2.64-1,4.17,4.17,0,0,1-1.83,2.31,8.39,8.39,0,0,0,2.39-.66A8.18,8.18,0,0,1,27.09,14.89Z"/>
                 </svg>
             </a>
-            <a class="icon-link" href="mailto: dallinis@hotmail.com">
+            <a class="icon-link" href="mailto: dallincoons@gmail.com">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
                     <title>email-icon</title>
                     <polygon class="cls-email-icon" points="14.05 18.55 14.05 29.45 19.5 24 14.05 18.55"/>
@@ -136,28 +120,13 @@ export default {
 html, body {
     max-width: 100%;
     overflow-x: hidden;
+    background: rgba(240,246,255,.3);
 }
 .main-wrapper {
     padding: 1rem;
     position: relative;
     width: 1000px;
     margin: 0 auto;
-}
-
-h1 {
-    font-size: 60px;
-    line-height: 55px;
-    font-weight: 700;
-    margin: 0;
-    margin-bottom: 1rem;
-    margin-left: -1rem;
-    transform: rotate(-6deg);
-}
-
-h2 {
-    margin: 0;
-    font-size: 16px;
-    transform: rotate(-6deg);
 }
 
 h3 {
@@ -172,10 +141,32 @@ ul {
 
 
 .header-section {
-    background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");
-    margin: -3rem;
-    padding: 3.5rem;
-    transform: rotate(6deg);
+    display: grid;
+    grid-template-columns: 1fr .15fr;
+    grid-template-rows: auto;
+    margin-top: 4rem;
+}
+
+.title {
+    color: #10abd1;
+    font-size: 60px;
+    line-height: 55px;
+    font-weight: 700;
+    margin: 0;
+    margin-bottom: 1rem;
+}
+
+.sub-title {
+    margin: 0;
+    font-size: 16px;
+}
+
+.sub-title-1 {
+    grid-row: 2;
+}
+
+.sub-title-2 {
+    grid-row: 3;
 }
 
 .section-title {
@@ -187,7 +178,7 @@ ul {
 }
 
 .section-wrapper {
-    margin-top: 5rem;
+    margin-top: 3rem;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: 1fr auto;
@@ -199,30 +190,44 @@ ul {
     border-bottom: 8px solid #01abd1;
     padding: .5rem;
     margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
 }
 
-.project-header {
-    display: grid;
-    grid-template-columns: 1fr .35fr;
-}
 
 .project-title {
     margin: 0;
     font-size: 1.2rem;
 }
 
-.project-links {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    justify-items: center;
-}
-
-.project-header svg {
-    width: 50%;
-}
-
 .project-description {
     line-height: 2;
+}
+
+.project:hover .project-more {
+    height: 56px;
+    opacity: 1;
+    transition: all .4s ease-in-out;
+}
+
+.project-more {
+    background: #10abd1;
+    opacity: 0;
+    color: #0d0d0d;
+    margin: -.5rem;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    font-weight: 600;
+    font-size: 1.2rem;
+    transition: all .4s ease-in-out;
+}
+
+
+.list-of-articles {
+    grid-column: 1 / span 3;
 }
 
 .article {
@@ -234,6 +239,13 @@ ul {
     text-decoration: none;
     color: #01ABD1;
     font-weight: 500;
+    transition: all .2s ease-in-out;
+}
+
+.article:hover a {
+    font-weight: 700;
+    color: #0d0d0d;
+    transition: all .2s ease-in-out;
 }
 
 .icon-wrapper {
@@ -258,6 +270,7 @@ ul {
     padding: 1rem 2rem;
     text-transform: uppercase;
     font-weight: 600;
+    text-align: center;
 }
 
 
@@ -266,6 +279,46 @@ ul {
     color: #0D0D0D;
     font-weight: 700;
     text-decoration: none;
+}
+
+.hvr-sweep-to-right {
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    position: relative;
+    -webkit-transition-property: color;
+    transition-property: color;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+}
+.hvr-sweep-to-right:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #10abd1;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transform-origin: 0 50%;
+    transform-origin: 0 50%;
+    -webkit-transition-property: transform;
+    transition-property: transform;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-timing-function: ease-out;
+    transition-timing-function: ease-out;
+}
+.hvr-sweep-to-right:hover, .hvr-sweep-to-right:focus, .hvr-sweep-to-right:active {
+    color: white;
+}
+.hvr-sweep-to-right:hover:before, .hvr-sweep-to-right:focus:before, .hvr-sweep-to-right:active:before {
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
 }
 
 .cls-github{fill:#0D0D0D;
@@ -294,14 +347,24 @@ footer {
 
 .icon-link {
     display: inline-block;
-    width: 20px;
-    height: 20px;
+    width: 25px;
+    height: 25px;
     margin-right: 1rem;
 }
 
 .icon-link svg{
     width: 100%;
     transform: rotate(-90deg);
+    transition: all .2s ease-in-out;
+}
+.icon-link:hover svg{
+    transform: rotate(-90deg) translateY(-3px);
+    transition: all .2s ease-in-out;
+}
+
+.icon-link:hover svg path {
+    fill: #0D0D0D;
+    transition: all .2s ease-in-out;
 }
 
 .email-wrapper {
@@ -317,6 +380,13 @@ footer {
     color: #10abd1;
     text-decoration: none;
     font-weight: 700;
+    transition: all .2s ease-in-out;
+}
+
+.email-wrapper:hover a {
+    color: #0d0d0d;
+    transform: translateX(-3px);
+    transition: all .2s ease-in-out;
 }
 
 .email-line {
@@ -326,25 +396,138 @@ footer {
     margin-left: 1rem;
 }
 
+@media (max-width: 1500px) {
+    .social-links-wrapper {
+        left: -200px;
+    }
+}
+
+@media (min-width: 1199px) and (max-width: 1400px) {
+    .social-links-wrapper {
+        left: -250px;
+    }
+
+    .email-wrapper {
+        right: -250px;
+    }
+}
+
+@media (max-width: 1200px) {
+    .main-wrapper {
+        width: 800px;
+    }
+
+    .social-links-wrapper {
+        left: -300px;
+    }
+
+    .email-wrapper {
+        right: -300px;
+    }
+}
+
 @media (max-width: 900px) {
+    .main-wrapper {
+        width: 70%;
+    }
+
+    .title {
+        font-size: 60px;
+        line-height: 55px;
+        font-weight: 700;
+        margin: 0;
+        margin-bottom: 1rem;
+        margin-left: -1rem;
+        transform: rotate(-6deg);
+        color: #0d0d0d;
+    }
+
+    .sub-title {
+        margin: 0;
+        font-size: 16px;
+        transform: rotate(-6deg);
+    }
+    .header-section {
+        /*background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");*/
+        /*background-size: cover;*/
+        /*background: #10abd1;*/
+        /*width: 100vw;*/
+        /*margin: -3rem;*/
+        /*padding: 3.5rem;*/
+        /*transform: rotate(6deg);*/
+    }
+
+    .lg-screen-resume {
+        display: none;
+    }
+
+    .section-wrapper {
+        grid-template-columns: 1fr;
+    }
+
+    .section-title {
+        grid-column: 1;
+    }
+
+    .email-wrapper, .social-links-wrapper {
+        display: none;
+    }
+
+    .icon-link svg{
+        transform: rotate(0);
+    }
+
     footer {
-        background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");
-        margin: 2rem -2rem -1rem -2rem;
-        padding: 2.5rem;
+        /*background-image: url("../assets/shapes-background.png"), url("../assets/blue-background.png");*/
+        /*margin: 2rem -2rem -1rem -2rem;*/
+        /*padding: 2.5rem;*/
+        margin-top: 2rem;
+        padding: 1rem 0;
         display: grid;
-        justify-items: center;
+        grid-template-columns: 1fr .25fr;
+        grid-template-rows: 1fr 1fr;
+        align-items: center;
+        /*justify-items: left;*/
         grid-gap: 10px;
+        border-top: 8px solid #10abd1;
+        transform: rotate(-4deg);
     }
     .footer-info {
         font-style: italic;
         font-size: 12px;
         text-align: center;
         margin-top: 1rem;
+        grid-column: 1 / span 2;
     }
     .footer-info a {
         color: #0D0D0D;
         font-weight: 700;
         text-decoration: none;
+    }
+
+    .icon-link {
+        width: 40px;
+        height: 40px;
+        margin: 0;
+    }
+
+    .resume-button-wrapper {
+        margin-top: 0;
+    }
+}
+
+@media (max-width: 500px) {
+    .main-wrapper {
+        width: 85%;
+    }
+
+    footer {
+        grid-template-rows: repeat(3, 1fr);
+        grid-template-columns: 1fr;
+    }
+
+    .resume-button-wrapper {
+        grid-row: 2;
     }
 }
 
